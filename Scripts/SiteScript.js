d5adbedcdd582d25331ae9a87c28aba6fb588fad
Connor <connor.gandy@justdigitaluk.com>
2023-05-30 14:23:24 +0100

    Checkbox changes and more tidy up

--- Scripts/SiteScript.js
+++ Scripts/SiteScript.js
@@ -1,4 +1,4 @@
-//NavBar hider (older version)
+//NavBar hider (Version 1 - faster response but more demanding)
 // var prevScrollpos = window.pageYOffset;
 // window.onscroll = function() 
 // {
@@ -7,14 +7,14 @@
 //     {
 //         document.getElementById("NavBanner").style.top = "0";
 //     } 
-//     else if(window.pageYOffset > 500 && window.matchMedia("(min-width: 1100px)").matches) 
+//     else if(window.pageYOffset > 500 && window.matchMedia("(min-width: 1100px)").matches) /*Check for minimum scrolled distance and mobile view*/
 //     {
 //         document.getElementById("NavBanner").style.top = "-10vh";
 //     }
 //     prevScrollpos = currentScrollPos;
 // }
 
-//NavBar hider (reduced polling version)
+//NavBar hider (Version 2 - more performant with a reduced polling rate)
 var pollingRate;
 var prevScrollpos = window.pageYOffset;
 window.addEventListener("scroll", function()
@@ -27,13 +27,13 @@ window.addEventListener("scroll", function()
         {
             document.getElementById("NavBanner").style.top = "0";
         } 
-        else if(window.pageYOffset > 500 && window.matchMedia("(min-width: 1100px)").matches) 
+        else if(window.pageYOffset > 500 && window.matchMedia("(min-width: 1100px)").matches) /*Check for minimum scrolled distance and mobile view*/
         {
             document.getElementById("NavBanner").style.top = "-10vh";
         }
         prevScrollpos = currentScrollPos;
     },
-    40)
+    40)//Polling rate in milliseconds
 })
 
 
